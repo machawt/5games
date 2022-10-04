5328725584823f5d7f1d55840b0d180465c3ea42
machawt <mark@holt5.com>
2022-10-04 16:07:01 +1300

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const userChoiceDisplay=document.getElementById ('user-choice');
 const resultDisplay=document.getElementById ('result');
 const possibleChoices=document.querySelectorAll('button')
 let userChoice
+let computerChoice
 possibleChoices.forEach(possibleChoice=>possibleChoice.addEventListener('click',(e)=>{
   userChoice=e.target.id
   userChoiceDisplay.innerHTML=userChoice
