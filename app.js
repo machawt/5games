f49f8898e7e4da42057e510fba7f1cebaba42c26
machawt <mark@holt5.com>
2022-10-04 14:49:32 +1300

    Update app.js

--- app.js
+++ app.js
@@ -6,4 +6,16 @@ let userChoice
 possibleChoices.forEach(possibleChoice=>possibleChoice.addEventListener('click',(e)=>{
   userChoice=e.target.id
   userCHoiceDisplay.innerHTML=userChoice
+  generateComputerChoice()
 }}}
+function generateComputerChoice(){
+  const randomNumber=Math.floor(Math.random()*3)+1
+  if (randomNumber===1){
+    computerChoice='rock'}
+  if (randomNumber===2){
+    computerChoice='paper'}
+  if (randomNumber===3){
+    computerChoice='scissors'}
+  
+  
+  }
