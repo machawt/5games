1b8c9dc447ac8d1e98c2b5dfa580203482680cb3
machawt <mark@holt5.com>
2022-10-10 17:32:12 +1300

    Update app.js

--- app.js
+++ app.js
@@ -8,17 +8,17 @@ let computerChoice
 possibleChoices.forEach(possibleChoice=>possibleChoice.addEventListener('click',(e)=>{
   userChoice=e.target.id
   userChoiceDisplay.innerHTML=userChoice
- // generateComputerChoice()
+  generateComputerChoice()
 getResult()
 }))
-//function generateComputerChoice(){
+function generateComputerChoice(){
  /* const randomNumber=Math.floor(Math.random()*3)+1
   if (randomNumber===1){
     computerChoice='ਕਿ੍ਸਟੀਨਾ'}
   if (randomNumber===2){
     computerChoice='ਅੰਮ੍ਰਿਤ'}
   if (randomNumber===3){
-    computerChoice='ਜੌਸ਼'}
+    computerChoice='ਜੌਸ਼'}*/
 
 
 possibleChoices2.forEach(possibleChoice=>possibleChoice.addEventListener('click',(e)=>{
@@ -41,7 +41,7 @@ function getResult(){
          if (computerChoice==='ਜੌਸ਼'&&userChoice==='ਅੰਮ੍ਰਿਤ')
     {result="You win!"}
     resultDisplay.innerHTML=result
-  }*/
+  }
 //<button id = "cj">ਕਿ੍ਸਟੀਨਾ</button>
 //<button id = "am">ਅੰਮ੍ਰਿਤ</button>
 //<button id = "josh">ਜੌਸ਼</button>
